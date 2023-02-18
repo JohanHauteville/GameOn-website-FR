03be1dd1bd914e5983ffe5d7fed89aba547e10b6
JohanHauteville <johan.hauteville@gmail.com>
2023-02-19 00:18:30 +0100

    Add checkString

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -17,10 +17,11 @@ const modalX = document.querySelector(".close");
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
 // Fermeture de la modal
-modalX.addEventListener("click", function(){
+modalX.addEventListener("click", async function(){
   // modalbg.style.opacity = "0";
-  // modalbg.style.transition = "opacity .5s";
+  // modalbg.style.transition = await "opacity .5s";
   modalbg.style.display = "none";
+  // modalbg.style.opacity = "1";
   
 })
 
@@ -29,4 +30,34 @@ function launchModal() {
   modalbg.style.display = "block";
   
 }
+
+function ajoutSpanError(idElement,text){
+  document.getElementById(idElement).innerText = text;
+}
+
+
+
+function checkString(inputID,name){
+  const lastID = document.getElementById(inputID).value;
+  if(lastID===""){    
+    ajoutSpanError("error"+inputID,"Votre "+name+" est manquant");
+  } else if (lastID.length<=2){
+    ajoutSpanError("error"+inputID,"Votre "+name+" doit contenir plus de deux caractères");
+  }else{
+    ajoutSpanError("error"+inputID,"");
+  }
+}
+
+
+
+
+function validate(){
+  event.preventDefault();
+  checkString("first","prénom");
+  checkString("last","nom");
+  console.log("validate");
+}
+
+
+
 
