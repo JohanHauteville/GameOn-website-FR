c78828941cd2b6087f490e70957cb0c68f1c6606
JohanHauteville <johan.hauteville@gmail.com>
2023-02-22 15:45:11 +0100

    Fix #3 #4

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -10,6 +10,7 @@ function editNav() {
 // DOM Elements
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
+const modalBtnSubmit = document.querySelector(".btn-submit");
 const formData = document.querySelectorAll(".formData");
 const modalX = document.querySelector(".close");
 
@@ -22,30 +23,52 @@ function launchModal() {
   
 }
 
+// close modal event
+modalX.addEventListener("click", closeModal);
 
-
-// Fermeture de la modal
-modalX.addEventListener("click", function(){
+//close modal form
+function closeModal(){
   modalbg.style.display = "none";
-})
-
-
-// Ajoute un message d'erreur sur le span choisi
-function ajoutSpanError(idElement,text){
-  const targetIdElement = "error"+idElement;
-  document.getElementById(targetIdElement).innerText = text;
 }
 
 
-// fonction de contrôle de chaine
-function checkString(inputID,name){
-  const lastID = document.getElementById(inputID).value;
+
+// fonction d'affichage d'un champs incorrect
+function affichageErreur(inputNumber=0,text=""){
+  if(text===""){    // Si la chaine est vide alors on supprime l'affichage de l'erreur
+    formData[inputNumber].removeAttribute("data-error-visible");
+    formData[inputNumber].removeAttribute("data-error");
+
+  } else {   // Sinon on affiche l'erreur avece le texte entré
+    formData[inputNumber].setAttribute("data-error-visible","true");
+    formData[inputNumber].setAttribute("data-error",text);
+
+  }
+
+}
+
+// fonction de contrôle du prénom
+function checkFirst(){
+  const lastID = document.getElementById("first").value;
   if(lastID===""){    // controle si la chaine est vide
-    ajoutSpanError(inputID,"Votre "+name+" est manquant");
+    affichageErreur(0,"Votre prénom est manquant");
   } else if (lastID.length<=2){   // controle si la chaine a plus de deux caractères
-    ajoutSpanError(inputID,"Votre "+name+" doit contenir plus de deux caractères");
+    affichageErreur(0,"Votre prénom doit contenir plus de deux caractères");
   }else{
-    ajoutSpanError(inputID,"");
+    affichageErreur(0,"");
+    return true;
+  }
+}
+
+// fonction de contrôle du nom
+function checkLast(){
+  const lastID = document.getElementById("last").value;
+  if(lastID===""){    // controle si la chaine est vide
+    affichageErreur(1,"Votre nom est manquant");
+  } else if (lastID.length<=2){   // controle si la chaine a plus de deux caractères
+    affichageErreur(1,"Votre nom doit contenir plus de deux caractères");
+  }else{
+    affichageErreur(1,"");
     return true;
   }
 }
@@ -56,64 +79,78 @@ function checkEmail(){
   const emailValue = document.getElementById("email").value;              // Récupération de l'adresse mail entrée par l'utilisateur
 
   if( /\@/.test(emailValue) === false){                    // Test de la présence de '@'
-    ajoutSpanError("email","'@' manquant");
+    affichageErreur(2,"Le '@' est manquant");
   } else if(/(\@)([a-zA-Z-]+)\.([a-zA-Z]{2,3})$/mg.test(emailValue) === false){     // Test de la présence du nom de domaine complet
-      ajoutSpanError("email","Texte après le '@' manquant ou incorrect");
+      affichageErreur(2,"Texte après le '@' manquant ou incorrect");
     }else if(regex.test(emailValue)){           // Vérification du reste de l'adresse email (avant le '@')
-      ajoutSpanError("email","");               // Efface le message d'erreur quand l'email est correct
+      affichageErreur(2,"");
       return true;
     }else{
-      ajoutSpanError("email","Texte avant le '@' manquant ou incorrect");
+      affichageErreur(2,"Texte avant le '@' manquant ou incorrect");
     }
   }
+
 // fonction de contrôle du nombre de concours
 function checkNumberOfConcour(){
     const numberOfConcourATester = document.getElementById("quantity").value;
     if (parseInt(numberOfConcourATester) >=0 && parseInt(numberOfConcourATester) <=99 ){  // Test d'une valeur se situant entre 0 et 99.
-      ajoutSpanError("quantity","");
+      affichageErreur(4,"");
       return true;
     } else{
-      ajoutSpanError("quantity","Votre nombre doit être compris entre 0 et 99."); // Si valeur incorrecte ou absente, affichage du message d'erreur.
+      affichageErreur(4,"Votre nombre doit être compris entre 0 et 99."); // Si valeur incorrecte ou absente, affichage du message d'erreur.
     }
   }
+
 // fonction de contrôle de la ville choisie
 function checkRadioCity(){
   const listOfRadio = document.getElementsByName("location");   // Récupère la liste des boutons radio
   let locationChecked = "";   // initialise la variable qui recevra la ville choisie
-
   for(let radioButton of listOfRadio){      // Parcour des buttons radio pour trouver lequel a été cochée
     if(radioButton.checked === true){
       locationChecked = radioButton.value;  // Attribution de la ville cochée
-      ajoutSpanError("radio","");
+      affichageErreur(5,"");
       return true;
     } 
   }
   if(locationChecked===""){
-    ajoutSpanError("radio","Veuillez choisir une ville");   // Si aucune ville cochée, affichage du message d'erreur
+    affichageErreur(5,"Veuillez choisir une ville"); // Si aucune ville cochée, affichage du message d'erreur
   }
 }
 
+// fonction de contrôle des conditions d'utilisation
 function checkCondition(){
   if(document.getElementById("checkbox1").checked){
-    ajoutSpanError("condition","");
+    affichageErreur(6,"");
     return true;
   } else {
-    ajoutSpanError("condition","Merci de bien vouloir lire et accepter les conditions d'utilisation");
+    affichageErreur(6,"Merci de bien vouloir lire et accepter les conditions d'utilisation");
   }
 }
 
+
+
+
+// fonction de validation du formulaire
 function validate(event){
+  event.preventDefault(); //désactive le comportement classique du formulaire
   if(
-    checkString("first","prénom") &
-    checkString("last","nom") &
+    checkFirst() &
+    checkLast() &
     checkEmail() &
     checkNumberOfConcour() &
     checkRadioCity() &
     checkCondition()
   ){
-    console.log("validation ok !");
+    for(let data of formData){
+      data.innerHTML="";
+    }
+    modalBtnSubmit.style.display="none";
+    formData[0].insertAdjacentHTML("beforebegin"," <div class=\"form-validated\"> <span class=\"form-validated-span\">Merci pour<br>votre inscription<span></div>");
+    formData[0].insertAdjacentHTML("beforebegin"," <a class=\"btn-close\" onclick=\"closeModal()\">Fermer</a>");
+    
   } else {
-    event.preventDefault(); 
+
+    
   }
 }
 
